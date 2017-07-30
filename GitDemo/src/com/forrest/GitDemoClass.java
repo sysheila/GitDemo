7d1dc250d41e26bbb4cbfd0c699228ebb9ef4e81
Kishore <kishore1634@gmail.com>
2017-07-31 03:52:54 +0800

    Here is the first change to GitDemo

--- GitDemo/src/com/forrest/GitDemoClass.java
+++ GitDemo/src/com/forrest/GitDemoClass.java
@@ -5,5 +5,6 @@ public class GitDemoClass {
 	public void ShowSms() {
 		System.out.println("Hello Phillipino friends!");
 		System.out.println("Welcome to China.");
+		System.out.println("First change here.");
 	}
 }
