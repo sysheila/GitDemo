298f459a6aa26ace430ac62e0244a193c152128e
Kishore <kishore1634@gmail.com>
2017-07-31 03:19:39 +0800

    This is the first commit.

---
+++ GitDemo/src/com/forrest/GitDemoClass.java
@@ -0,0 +1,9 @@
+package com.forrest;
+
+public class GitDemoClass {
+
+	public void ShowSms() {
+		System.out.println("Hello Phillipino friends!");
+		System.out.println("Welcome to China.");
+	}
+}
---
+++ GitDemo/src/com/forrest/GitMain.java
@@ -0,0 +1,12 @@
+package com.forrest;
+
+public class GitMain {
+
+	public static void main(String[] args) {
+		
+		GitDemoClass obj = new GitDemoClass();
+		obj.ShowSms();
+
+	}
+
+}
